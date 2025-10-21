e9b536f1de3d3b703e9485279ef1c8dceda624e6
khoamai291299 <khoamai2912@gmail.com>
2025-10-21 10:49:22 +0700

    feat: Add function Completed Task

--- app.py
+++ app.py
@@ -6,17 +6,28 @@ def add_task(task_name):
     print(f"Đã thêm công việc: '{task_name}'")
 
 def list_task(tasks):
+    for i in tasks:
+        if i["completed"] == "True":
+            print(f'[x] {i["name"]}')
+        else:
+            print(f'[ ] {i["name"]}')
+
+def complete_task(task_index):
     for i in range(len(tasks)):
-        print(f'{i+1}. {tasks[i]}')
+        if (task_index - 1 == i):
+            tasks[i][1] = "True"
 
 # --- Điểm bắt đầu của chương trình ---
 if __name__ == "__main__":
     print("Chào mừng đến với ứng dụng To-Do List!")
-    add_task("Học bài Git và GitHub")
-    add_task("Làm bài tập thực hành ở nhà")
-    add_task("Học bài Git")
-    add_task("Làm bài tập")
+    for i in range(2):
+        name = input('Nhập tên công việc: ')
+        completed = input('Nhập trạng thái hoàn thành: ')
 
+        dic = {"name" : name, "completed" : completed}
+        add_task(dic)
+    
+    complete_task(1)
     list_task(tasks)
 
 
